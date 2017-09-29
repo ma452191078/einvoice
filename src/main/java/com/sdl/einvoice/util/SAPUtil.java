0eb819e03b53686e9c27332c716db6e92a32398b
Code <majingyuanno.1@gmail.com>
2017-09-29 15:53:03 +0800

    增加连接方法

--- src/main/java/com/sdl/einvoice/util/SAPUtil.java
+++ src/main/java/com/sdl/einvoice/util/SAPUtil.java
@@ -3,9 +3,14 @@ package com.sdl.einvoice.util;
 import com.sap.conn.jco.JCoDestination;
 import com.sap.conn.jco.JCoDestinationManager;
 import com.sap.conn.jco.JCoException;
+import com.sap.conn.jco.ext.DestinationDataProvider;
 import com.sdl.einvoice.config.SapConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Properties;
 
 /**
  * SAP连接工具类
@@ -15,23 +20,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class SAPUtil {
 
     @Autowired
-    private SapConfig sapConfig;
+    private static SapConfig sapConfig;
 
-    public JCoDestination getDestination() throws JCoException
-    {
-        /**
-         * Get instance of JCoDestination from file: ECC.jcodestination
-         * which should be located in the installation folder of project
-         */
+    static String ABAP_AS_POOLED = "ABAP_AS_WITH_POOL";
 
-        JCoDestination dest = JCoDestinationManager.getDestination("/src/ECC");
-        return dest;
+    public static Properties createProperties(String clientName){
+
+        Properties connectProperties = new Properties();
+        connectProperties.setProperty(DestinationDataProvider.JCO_ASHOST, sapConfig.getAshost());
+        connectProperties.setProperty(DestinationDataProvider.JCO_SYSNR,  sapConfig.getSysnr());
+        connectProperties.setProperty(DestinationDataProvider.JCO_CLIENT, sapConfig.getClient());
+        connectProperties.setProperty(DestinationDataProvider.JCO_USER,   sapConfig.getUser());
+        connectProperties.setProperty(DestinationDataProvider.JCO_PASSWD, sapConfig.getPasswd());
+        connectProperties.setProperty(DestinationDataProvider.JCO_LANG,   sapConfig.getLang());
+        createDataFile(clientName, "jcoDestination", connectProperties);
+        return connectProperties;
     }
 
 
+    private static void createDataFile(String name, String suffix, Properties properties)
+    {
+        File cfg = new File(name+"."+suffix);
+        if(!cfg.exists())
+        {
+            try
+            {
+                FileOutputStream fos = new FileOutputStream(cfg, false);
+                properties.store(fos, "for tests only !");
+                fos.close();
+
+            }
+            catch (Exception e)
+            {
+                throw new RuntimeException("Unable to create the destination file " + cfg.getName(), e);
+
+            }
+        }
+    }
+
     @Test
-    public void pingDestination() throws JCoException {
-        JCoDestination dest = this.getDestination();
-        dest.ping();
+    public static void step1Connect() throws JCoException
+    {
+        Properties conPro = createProperties(ABAP_AS_POOLED);
+        JCoDestination destination = JCoDestinationManager.getDestination(ABAP_AS_POOLED);
+        destination.ping();
+        System.out.println("Attributes:");
+        System.out.println(destination.getAttributes());
+        System.out.println();
     }
 }
