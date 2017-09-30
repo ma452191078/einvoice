fe9ea96aa8103f347d5139e84e839d673d501305
majingyuan <majingyuanno.1@gmail.com>
2017-09-30 08:50:15 +0800

    sap访问测试通过，增加表参数请求

--- src/main/java/com/sdl/einvoice/util/SAPUtil.java
+++ src/main/java/com/sdl/einvoice/util/SAPUtil.java
@@ -1,8 +1,6 @@
 package com.sdl.einvoice.util;
 
-import com.sap.conn.jco.JCoDestination;
-import com.sap.conn.jco.JCoDestinationManager;
-import com.sap.conn.jco.JCoException;
+import com.sap.conn.jco.*;
 import com.sap.conn.jco.ext.DestinationDataProvider;
 import com.sdl.einvoice.config.SapConfig;
 import org.junit.jupiter.api.Test;
@@ -19,22 +17,28 @@ import java.util.Properties;
  */
 public class SAPUtil {
 
-    @Autowired
-    private static SapConfig sapConfig;
-
     static String ABAP_AS_POOLED = "ABAP_AS_WITH_POOL";
 
-    public static Properties createProperties(String clientName){
+    public SAPUtil(SapConfig sapConfig){
 
         Properties connectProperties = new Properties();
-        connectProperties.setProperty(DestinationDataProvider.JCO_ASHOST, sapConfig.getAshost());
-        connectProperties.setProperty(DestinationDataProvider.JCO_SYSNR,  sapConfig.getSysnr());
-        connectProperties.setProperty(DestinationDataProvider.JCO_CLIENT, sapConfig.getClient());
-        connectProperties.setProperty(DestinationDataProvider.JCO_USER,   sapConfig.getUser());
-        connectProperties.setProperty(DestinationDataProvider.JCO_PASSWD, sapConfig.getPasswd());
-        connectProperties.setProperty(DestinationDataProvider.JCO_LANG,   sapConfig.getLang());
-        createDataFile(clientName, "jcoDestination", connectProperties);
-        return connectProperties;
+//        connectProperties.setProperty(DestinationDataProvider.JCO_ASHOST, sapConfig.getAshost());
+//        connectProperties.setProperty(DestinationDataProvider.JCO_SYSNR,  sapConfig.getSysnr());
+//        connectProperties.setProperty(DestinationDataProvider.JCO_CLIENT, sapConfig.getClient());
+//        connectProperties.setProperty(DestinationDataProvider.JCO_USER,   sapConfig.getUser());
+//        connectProperties.setProperty(DestinationDataProvider.JCO_PASSWD, sapConfig.getPasswd());
+//        connectProperties.setProperty(DestinationDataProvider.JCO_LANG,   sapConfig.getLang());
+
+        connectProperties.setProperty(DestinationDataProvider.JCO_ASHOST, "192.168.7.11");
+        connectProperties.setProperty(DestinationDataProvider.JCO_SYSNR,  "00");
+        connectProperties.setProperty(DestinationDataProvider.JCO_CLIENT, "202");
+        connectProperties.setProperty(DestinationDataProvider.JCO_USER,   "majy");
+        connectProperties.setProperty(DestinationDataProvider.JCO_PASSWD, "20140254");
+        connectProperties.setProperty(DestinationDataProvider.JCO_LANG,   "zh");
+        connectProperties.setProperty(DestinationDataProvider.JCO_POOL_CAPACITY, "3");  //最大空连接数
+        connectProperties.setProperty(DestinationDataProvider.JCO_PEAK_LIMIT,    "10"); //最大活动连接数
+        createDataFile("ABAP_AS_POOLED", "jcoDestination", connectProperties);
+
     }
 
 
@@ -58,14 +62,92 @@ public class SAPUtil {
         }
     }
 
-    @Test
-    public static void step1Connect() throws JCoException
+
+    /**
+     * 创建sap连接
+     * @throws JCoException
+     */
+    static void createConnect() throws JCoException
     {
-        Properties conPro = createProperties(ABAP_AS_POOLED);
+//        Properties conPro = createProperties(ABAP_AS_POOLED);
+
         JCoDestination destination = JCoDestinationManager.getDestination(ABAP_AS_POOLED);
         destination.ping();
         System.out.println("Attributes:");
         System.out.println(destination.getAttributes());
         System.out.println();
+    }
+
+
+    public static void executeWithTable(String funName, String importName, String exportName, String tableName) throws JCoException
+    {
+        JCoDestination destination = JCoDestinationManager.getDestination(ABAP_AS_POOLED);
+        JCoFunction function = destination.getRepository().getFunction(funName);
+        if(function == null)
+            throw new RuntimeException(funName + " not found in SAP.");
+
+        try
+        {
+            function.execute(destination);
+        }
+        catch(AbapException e)
+        {
+            System.out.println(e.toString());
+            return;
+        }
+
+        JCoStructure returnStructure = function.getExportParameterList().getStructure(exportName);
+        if (! (returnStructure.getString("TYPE").equals("")||returnStructure.getString("TYPE").equals("S"))  )
+        {
+            throw new RuntimeException(returnStructure.getString("MESSAGE"));
+        }
+
+        JCoTable codes = function.getTableParameterList().getTable("COMPANYCODE_LIST");
+        for (int i = 0; i < codes.getNumRows(); i++)
+        {
+            codes.setRow(i);
+            System.out.println(codes.getString("COMP_CODE") + '\t' + codes.getString("COMP_NAME"));
+        }
+
+        codes.firstRow();
+        for (int i = 0; i < codes.getNumRows(); i++, codes.nextRow())
+        {
+            function = destination.getRepository().getFunction("BAPI_COMPANYCODE_GETDETAIL");
+            if (function == null)
+                throw new RuntimeException("BAPI_COMPANYCODE_GETDETAIL not found in SAP.");
+
+            function.getImportParameterList().setValue("COMPANYCODEID", codes.getString("COMP_CODE"));
+            function.getExportParameterList().setActive("COMPANYCODE_ADDRESS",false);
+
+            try
+            {
+                function.execute(destination);
+            }
+            catch (AbapException e)
+            {
+                System.out.println(e.toString());
+                return;
+            }
+
+            returnStructure = function.getExportParameterList().getStructure("RETURN");
+            if (! (returnStructure.getString("TYPE").equals("") ||
+                    returnStructure.getString("TYPE").equals("S") ||
+                    returnStructure.getString("TYPE").equals("W")) )
+            {
+                throw new RuntimeException(returnStructure.getString("MESSAGE"));
+            }
+
+            JCoStructure detail = function.getExportParameterList().getStructure("COMPANYCODE_DETAIL");
+
+            System.out.println(detail.getString("COMP_CODE") + '\t' +
+                    detail.getString("COUNTRY") + '\t' +
+                    detail.getString("CITY"));
+        }//for
+    }
+
+    public static void main(String[] args) throws JCoException
+    {
+        SAPUtil sapUtil = new SAPUtil(null);
+        createConnect();
     }
 }
