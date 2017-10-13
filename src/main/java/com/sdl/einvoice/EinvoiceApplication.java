47f8ca818db786ac90d2207db22633797ff709aa
Code <majingyuanno.1@gmail.com>
2017-10-13 09:39:03 +0800

    修改为war包，配置tomcat

--- src/main/java/com/sdl/einvoice/EinvoiceApplication.java
+++ src/main/java/com/sdl/einvoice/EinvoiceApplication.java
@@ -2,10 +2,17 @@ package com.sdl.einvoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-public class EinvoiceApplication {
+public class EinvoiceApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EinvoiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EinvoiceApplication.class, args);
